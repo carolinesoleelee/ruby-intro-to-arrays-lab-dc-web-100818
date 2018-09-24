4ff4ec9e11c2b60df26abbee53b4da8c8beebc0f
Solee Lee <carolinesoleelee@gmail.com>
2018-09-24 20:35:22 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+def instantiate_new_array
+  Array[]
+end
+
+def array_with_two_elements
+  Array["one","two"]
+end
+
+def first_element(my_first_element)
+  Array[]
+  my_first_element[0]
+end
+
+def third_element(my_third_element)
+  Array[]
+ my_third_element[2]
+end
+
+def last_element(last_element)
+  Array[]
+  last_element[3]
+end
+
+def first_element_with_array_methods(first_country)
+  Array[]
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  Array[]
+  last_country.last
+end
+
+def length_of_array(length_of_array)
+   Array[]
+   length_of_array.length
+end
